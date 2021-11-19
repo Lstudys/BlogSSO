a3450b5e44e10c49692218ad7b142b48841e3b69
Lstudys <Lstudys@163.com>
2021-11-19 21:14:55 +0800

    初始化项目结构，添加头部组件

---
+++ src/components/layout/footer/index.vue
@@ -0,0 +1,7 @@
+<!--
+*  @FileDescription: Footer组件
+*  @Author: 李永晖
+*  @CreatedDate:2021/11/17
+*  @LastEditors: 李永晖
+*  @LastEditTime: 2021/11/19
+-->
---
+++ src/components/layout/header/index.vue
@@ -0,0 +1,45 @@
+<!--
+*  @FileDescription: Header组件
+*  @Author: 李永晖
+*  @CreatedDate:2021/11/17
+*  @LastEditors: 李永晖
+*  @LastEditTime: 2021/11/19
+-->
+
+<template>
+    <div class="my-header">
+        <div class="logo"><img src="../../../assets/images/logo.png" alt=""></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+<style scoped>
+body{
+  margin: 0;
+  padding: 0;
+  font-family: "微软雅黑";
+}
+
+.my-header{
+  width: 100%;
+  height: 80px;
+  border-top: 3px solid #EF6520;
+  z-index: 999;
+}
+.my-header .logo{
+  /* width: 1200px; */
+  margin: 0 auto;
+  overflow: hidden;
+  margin-top: 15px;
+  padding-left: 40px;
+}
+
+.my-header img{
+  height: 50px;
+}
+</style>
