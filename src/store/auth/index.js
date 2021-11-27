91dc9a4dc92943a0a5b5f9b55156db8ec58b24d7
Lstudys <Lstudys@163.com>
2021-11-27 17:35:17 +0800

    将用户信息、访问令牌和刷新令牌保存至cookie中

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -7,6 +7,7 @@ import {login} from '../../api/auth.js';
 
 import {cookie, key} from '../../utils/cookie.js';
 
+// 初始化时获取cookie信息
 const state = {
     userInfo:cookie.get(key.userInfokey) ? JSON.parse(cookie.get(key.userInfokey)):null,
     accessToken: cookie.get(key.accessTokenKey),
@@ -16,13 +17,27 @@ const state = {
 const mutations = {
 
     // 设置用户状态
-    SET_USER_STATE(){
-       
+    SET_USER_STATE(states = state,{userInfo,access_token,refresh_token}){
+        console.log(userInfo);
+       states.userInfo = userInfo;
+       states.accessToken = access_token;
+       states.refreshToken = refresh_token;
+
+       cookie.set(key.userInfokey,userInfo);
+       cookie.set(key.accessTokenKey,access_token);
+       cookie.set(key.refreshTokenKey,refresh_token);
     },
 
     // 重置用户状态
-    RESET_USER_STATE(){
+    RESET_USER_STATE(states = state){
 
+        states.userInfo = null;
+        states.accessToken = null;
+        states.refreshToken = null;
+
+        cookie.remove(key.userInfokey);
+        cookie.remove(key.accessTokenKey);
+        cookie.remove(key.refreshTokenKey);
     }
 }
 
@@ -30,14 +45,18 @@ const mutations = {
 const actions = {
 
 
-    UserLogin(data){
-        const {username,password} = data;
+    UserLogin({commit},userData){
+        const {username,password} = userData;
         login({username:username,password:password}).then(
             response =>{
-                const data = JSON.stringify(response);
-                console.log(data + '-------');
+                const {data} = response;
+                // console.log(JSON.stringify(response));
+                commit('SET_USER_STATE',data);
             }
-        )
+        ).catch(error =>{
+            console.log(error);
+            commit('RESET_USER_STATE')
+        })
     }
 }
 
